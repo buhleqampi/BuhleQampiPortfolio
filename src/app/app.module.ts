e62cb392bc9fc8dee9ede541223ea2ff0cce6a43
buhleqampi <buhle3857@gmail.com>
2024-06-07 15:40:15 +0200

    Old changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './Home/home/home.component';
-import { AboutComponent } from './About/about/about.component';
 import { ContactComponent } from './Contact/contact/contact.component';
 import { ProjectsComponent } from './Projects/projects/projects.component';
 import { EducationComponent } from './Education/education/education.component';
@@ -16,8 +14,6 @@ import { HeaderComponent } from './Header/header/header.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    AboutComponent,
     ContactComponent,
     ProjectsComponent,
     EducationComponent,
