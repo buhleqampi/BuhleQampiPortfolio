7f1857caa412c1e5a8cf3cf16903f97ad98e121c
buhleqampi <buhle3857@gmail.com>
2024-07-23 14:05:17 +0200

    changes-start over

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { ContactComponent } from './Contact/contact/contact.component';
-import { ProjectsComponent } from './Projects/projects/projects.component';
-import { EducationComponent } from './Education/education/education.component';
-
 
 const routes: Routes = [
-  {path: '', redirectTo:'home' , pathMatch: 'full'},
-  {path: 'contact', component: ContactComponent},
-  {path: 'education', component: EducationComponent},
-  {path: 'projects', component: ProjectsComponent}
+  {path: '', redirectTo:'home' , pathMatch: 'full'}
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ContactComponent } from './Contact/contact/contact.component';
-import { ProjectsComponent } from './Projects/projects/projects.component';
-import { EducationComponent } from './Education/education/education.component';
-import { FooterComponent } from './Footer/footer/footer.component';
-import { HeaderComponent } from './Header/header/header.component';
+
 
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ContactComponent,
-    ProjectsComponent,
-    EducationComponent,
-    FooterComponent,
-    HeaderComponent
+    AppComponent
+ 
   ],
   imports: [
     BrowserModule,
