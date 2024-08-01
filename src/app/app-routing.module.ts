a84b9a0852efc14ec68232d841374133067f8be1
buhleqampi <buhle3857@gmail.com>
2024-08-01 17:34:38 +0200

    day 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
 
 const routes: Routes = [
   {path: '', redirectTo:'home' , pathMatch: 'full'}
+  // {path : 'portfolio', component: PortfolioComponent},
+
 
 ];
 
