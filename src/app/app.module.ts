d21eae65ea2bf5663ca4a0d303f30bb2cdcbb85f
buhleqampi <buhle3857@gmail.com>
2024-08-04 11:42:27 +0200

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { BannerComponent } from './components/banner/banner.component';
-import { AboutComponent } from './components/about/about.component';
-import { WorkComponent } from './components/work/work.component';
-import { TechStackComponent } from './components/tech-stack/tech-stack.component';
-import { ProjectsComponent } from './components/projects/projects.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { PortfolioComponent } from './components/portfolio/portfolio.component';
-
 
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    BannerComponent,
-    AboutComponent,
-    WorkComponent,
-    TechStackComponent,
-    ProjectsComponent,
-    ContactComponent,
-    PortfolioComponent
+    AppComponent
  
   ],
   imports: [
