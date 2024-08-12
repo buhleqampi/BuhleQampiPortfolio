7f3447a5f96341f91ce1f796b7ad90c5c0791441
buhleqampi <buhle3857@gmail.com>
2024-08-13 00:22:13 +0200

    logic sana iyapopa

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  contactForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.contactForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      subject: ['', Validators.required],
+      message: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log('Form Submitted', this.contactForm.value);
+      // You can also add additional logic to handle form submission here
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  titles: string[] = ['Junior Software Engineer', 'Full Stack Web Developer'];
+  currentTitleIndex: number = 0;
+  currentTitle: string = this.titles[this.currentTitleIndex];
 
-  title="";
-  
+  constructor() {}
 
+  ngOnInit(): void {
+    this.startTitleRotation();
+  }
+
+  startTitleRotation(): void {
+    setInterval(() => {
+      this.currentTitleIndex = (this.currentTitleIndex + 1) % this.titles.length;
+      this.currentTitle = this.titles[this.currentTitleIndex];
+    }, 3000); 
+  }
 }
+
