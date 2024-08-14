f9f8b4d96ffa1d9bf05a799d6ac5292d07d7e618
buhleqampi <buhle3857@gmail.com>
2024-08-14 18:15:08 +0200

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,5 +9,19 @@ module.exports = {
   plugins: [
     require('flowbite/plugin')
   ],
+
+  screens: {
+    'smartphone': '378px',
+    // => @media (max-width:378px) { ......}
+    
+    'tablet': '640px',
+    // => @media (min-width: 640px) { ... }
+
+    'laptop': '1024px',
+    // => @media (min-width: 1024px) { ... }
+
+    'desktop': '1280px',
+    // => @media (min-width: 1280px) { ... }
+  }
 }
 
